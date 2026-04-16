afedbba87194ea48ddaf77545548acda5b2060e3
Dobó Heléna Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-16 15:39:02 +0200

    Listbox beállítása és user hozzáadás textboxokon keresztül

--- UserMaintance/UserMaintance/Form1.Designer.cs
+++ UserMaintance/UserMaintance/Form1.Designer.cs
@@ -28,21 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
+            listUsers = new ListBox();
             lblLastName = new Label();
             lblFirstName = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            btn = new Button();
+            txtLastName = new TextBox();
+            txtFirstName = new TextBox();
+            btnAdd = new Button();
             SuspendLayout();
             // 
-            // listBox1
+            // listUsers
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(41, 112);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(518, 820);
-            listBox1.TabIndex = 0;
+            listUsers.FormattingEnabled = true;
+            listUsers.Location = new Point(41, 112);
+            listUsers.Name = "listUsers";
+            listUsers.Size = new Size(518, 820);
+            listUsers.TabIndex = 0;
             // 
             // lblLastName
             // 
@@ -62,40 +62,40 @@
             lblFirstName.TabIndex = 2;
             lblFirstName.Text = "label2";
             // 
-            // textBox1
+            // txtLastName
             // 
-            textBox1.Location = new Point(981, 136);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(431, 55);
-            textBox1.TabIndex = 3;
+            txtLastName.Location = new Point(981, 136);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(431, 55);
+            txtLastName.TabIndex = 3;
             // 
-            // textBox2
+            // txtFirstName
             // 
-            textBox2.Location = new Point(981, 309);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(431, 55);
-            textBox2.TabIndex = 4;
+            txtFirstName.Location = new Point(981, 309);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(431, 55);
+            txtFirstName.TabIndex = 4;
             // 
-            // btn
+            // btnAdd
             // 
-            btn.Location = new Point(770, 438);
-            btn.Name = "btn";
-            btn.Size = new Size(642, 69);
-            btn.TabIndex = 5;
-            btn.Text = "button1";
-            btn.UseVisualStyleBackColor = true;
+            btnAdd.Location = new Point(770, 438);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(642, 69);
+            btnAdd.TabIndex = 5;
+            btnAdd.Text = "button1";
+            btnAdd.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(20F, 48F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1455, 1040);
-            Controls.Add(btn);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(btnAdd);
+            Controls.Add(txtFirstName);
+            Controls.Add(txtLastName);
             Controls.Add(lblFirstName);
             Controls.Add(lblLastName);
-            Controls.Add(listBox1);
+            Controls.Add(listUsers);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -104,11 +104,11 @@
 
         #endregion
 
-        private ListBox listBox1;
+        private ListBox listUsers;
         private Label lblLastName;
         private Label lblFirstName;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Button btn;
+        private TextBox txtLastName;
+        private TextBox txtFirstName;
+        private Button btnAdd;
     }
 }
--- UserMaintance/UserMaintance/Form1.cs
+++ UserMaintance/UserMaintance/Form1.cs
@@ -1,13 +1,30 @@
+using System.ComponentModel;
+using UserMaintance.Entities;
+
 namespace UserMaintance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+            // Localizáció beállítása
             lblFirstName.Text = Resource1.FirstName;
             lblLastName.Text = Resource1.LastName;
-            btn.Add.Text = Resource1.Add;
+            btnAdd.Text = Resource1.Add;
+
+            // listbox beállítások
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+
+            var u = new User
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+            users.Add(u);
         }
     }
 }
