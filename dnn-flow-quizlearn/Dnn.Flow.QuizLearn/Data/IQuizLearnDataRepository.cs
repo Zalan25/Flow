54e1fdf029f77fb19e4478f18a46e1e4568e677e
Dobó H. Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-21 23:39:25 +0200

    dataprovider elkészült

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Data/IQuizLearnDataRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Dnn.Flow.QuizLearn.Models;
+
+namespace Dnn.Flow.QuizLearn.Data
+{
+    public interface IQuizLearnDataRepository
+    {
+        // Lookups
+        IEnumerable<LanguageInfo> GetAllActiveLanguages();
+        IEnumerable<QuestionLevelInfo> GetAllQuestionLevels();
+        IEnumerable<SkillTypeInfo> GetAllSkillTypes();
+        IEnumerable<PaceTypeInfo> GetAllPaceTypes();
+        AssessmentModeInfo GetAssessmentModeByKey(string modeKey);
+
+        // Sessions
+        int AddAssessmentSession(AssessmentSessionInfo sessionInfo);
+        int AddAssessmentSessionSkill(int moduleId, int assessmentSessionId, int skillTypeId);
+        bool CompleteAssessmentSession(int moduleId, int assessmentSessionId, int? finalLevelId);
+
+        // Recommendation rules
+        IEnumerable<RecommendationRuleInfo> FindExactRecommendationRules(
+            int moduleId,
+            int languageId,
+            int questionLevelId,
+            int skillTypeId,
+            int paceTypeId,
+            int? secondaryLanguageId);
+
+        IEnumerable<RecommendationRuleInfo> FindFallbackRecommendationRules(
+            int moduleId,
+            int languageId,
+            int questionLevelId,
+            int skillTypeId,
+            int? secondaryLanguageId);
+
+        IEnumerable<RecommendationRuleInfo> FindGeneralRecommendationRules(
+            int moduleId,
+            int languageId);
+
+        // Recommendation results
+        int AddRecommendationResult(RecommendationResultInfo resultInfo);
+        int AddRecommendationResultItem(RecommendationResultItemInfo itemInfo);
+
+
+    }
+}
