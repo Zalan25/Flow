8819fd7f5a269b85c0310cc3966cb1e0342b1aa9
Dobó H. Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-22 01:43:39 +0200

    SkillTypeInfo.cs

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Models/SkillTypeInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Dnn.Flow.QuizLearn.Models
+{
+    public class SkillTypeInfo
+    {
+        public int SkillTypeId { get; set; }
+        public string SkillName { get; set; }
+    }
+
