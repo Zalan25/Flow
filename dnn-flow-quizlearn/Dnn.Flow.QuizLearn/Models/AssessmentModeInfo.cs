e19a191c5719bc88d96115d203f051ed79d6597e
Dobó H. Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-22 01:46:30 +0200

    AssessmentModeInfo.cs

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Models/AssessmentModeInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dnn.Flow.QuizLearn.Models
+{
+    public class AssessmentModeInfo
+    {
+        public int AssessmentModeId { get; set; }
+        public string ModeKey { get; set; }
+        public string ModeName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
