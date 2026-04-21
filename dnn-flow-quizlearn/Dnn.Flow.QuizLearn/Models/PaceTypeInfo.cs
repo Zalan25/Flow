6284f2bf5e1c5089c43b0ff1322fc3f09866dadf
Dobó H. Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-22 01:45:11 +0200

    PaceTypeInfo.cs

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Models/PaceTypeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dnn.Flow.QuizLearn.Models
+{
+
+    public class PaceTypeInfo
+    {
+        public int PaceTypeId { get; set; }
+        public int PaceKey { get; set; }
+        public string PaceName { get; set; }
+        public int MinProducts { get; set; }
+        public int MaxProducts { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
