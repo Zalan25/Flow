b624425f6c20b50b86e981a1dd582b2a4fda3003
Dobó H. Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-22 01:42:24 +0200

    QuestionLevelInfo.cs

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Models/QuestionLevelInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Dnn.Flow.QuizLearn.Models
+{
+    public class QuestionLevelInfo
+    {
+        public int QuestionLevelId { get; set; }
+        public string LevelName { get; set; }
+        public string LevelOrder { get; set; }
+    }
+
