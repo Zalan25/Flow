c926328fcd2887f5174ac0ec179718bd7059fe6b
Dobó Heléna Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-22 12:01:24 +0200

    services lookup megírás ez vezeti át az sql-ből az adatokat

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Services/Interfaces/ILookupService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Dnn.Flow.QuizLearn.Models;
+
+namespace Dnn.Flow.QuizLearn.Services.Interfaces
+{
+    public interface ILookupService
+    {
+        IEnumerable<LanguageInfo> GetLanguages();
+        IEnumerable<QuestionLevelInfo> GetLevels();
+        IEnumerable<SkillTypeInfo> GetSkills();
+        IEnumerable<PaceTypeInfo> GetPaceTypes();
+        AssessmentModeInfo GetAssessmentMode(string modeKey);
+    }
+}
