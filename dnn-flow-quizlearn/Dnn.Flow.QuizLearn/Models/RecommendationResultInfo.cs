1c953a3c5124fce350668fa90dc8f95758b81487
Dobó H. Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-22 02:00:05 +0200

    RecommendationResultInfo.cs

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Models/RecommendationResultInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dnn.Flow.QuizLearn.Models
+{
+    public class RecommendationResultInfo
+    {
+        public int ModuleId { get; set; }
+        public int AssessmentSessionId { get; set; }
+        public string SourceType { get; set; }
+        public int? RecommendedLevelId { get; set; }
+        public string? RecommendationSummary { get; set; }
+        public int MinProducts { get; set; }
+        public int MaxProducts { get; set; }
+    }
+}
--- dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Models/RecommendationRuleInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Dnn.Flow.QuizLearn.Models
-{
-    public class RecommendationRuleInfo
-    {
-
-    }
-}
