5fb3be8ecfc869c27a531d7c16535f16cc5dd02c
Dobó H. Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-23 12:00:52 +0200

    AssessmentService és az interface elkeszült a pontozási logikával

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Services/AssessmentService.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using Dnn.Flow.QuizLearn.Data;
+using Dnn.Flow.QuizLearn.Models;
+using Dnn.Flow.QuizLearn.Services.Interfaces;
+
+namespace Dnn.Flow.QuizLearn.Services
+{
+    public class AssessmentService : IAssessmentService
+    {
+        private readonly IQuizLearnDataRepository _repository;
+        public AssessmentService()
+        {
+            _repository = new SqlDataProvider();
+        }
+
+        public int StartAssessmentSession(AssessmentSessionInfo sessionInfo, IEnumerable<int> selectedSkillTypeIds)
+        {
+            var assessmentSessionId = _repository.AddAssessmentSession(sessionInfo);
+
+            foreach (var skillTypeId in selectedSkillTypeIds)
+            {
+                _repository.AddAssessmentSessionSkill(
+                    sessionInfo.ModuleId,
+                    assessmentSessionId,
+                    skillTypeId);
+            }
+
+            return assessmentSessionId;
+        }
+
+        public int CompleteAssessmentSession(int moduleId, int assessmentSessionId, int? finalLevelId)
+        {
+            return _repository.CompleteAssessmentSession(moduleId, assessmentSessionId, finalLevelId);
+        }
+
+        public int? DetermineFinalLevel(int a1CorrectCount, int a2CorrectCount, int b1CorrectCount, int b2CorrectCount, int c1CorrectCount)
+        {
+            if (a1CorrectCount < 3)
+            {
+                return 1; // A1
+            }
+
+            if (a2CorrectCount < 3)
+            {
+                return 2; // A2
+            }
+
+            if (b1CorrectCount < 3)
+            {
+                return 3; // B1
+            }
+
+            if (b2CorrectCount < 3)
+            {
+                return 4; // B2
+            }
+
+            if (c1CorrectCount < 3)
+            {
+                return 4; // C1
+            }
+            return 5;
+        }
+    }
+}
---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Services/Interfaces/IAssessmentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Dnn.Flow.QuizLearn.Models;
+
+namespace Dnn.Flow.QuizLearn.Services.Interfaces
+{
+    public interface IAssessmentService
+    {
+        int StartAssessmentSession(AssessmentSessionInfo sessionInfo, IEnumerable<int> selectedSkillTypeIds);
+
+        int CompleteAssessmentSession(int moduleId, int assessmentSessionId, int? finalLevelId);
+
+        int? DetermineFinalLevel(int a1CorrectCount, int a2CorrectCount, int b1CorrectCount, int b2CorrectCount, int c1CorrectCount);
+    }
+}
