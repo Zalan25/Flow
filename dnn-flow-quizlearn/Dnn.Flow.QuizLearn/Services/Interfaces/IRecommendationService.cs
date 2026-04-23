74d761966d89c81570715c8b95ffae6a996eaff1
Dobó H. Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-23 12:08:05 +0200

    RecommendationService kiépítése interface-el együtt

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Services/Interfaces/IRecommendationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Dnn.Flow.QuizLearn.Models;
+
+namespace Dnn.Flow.QuizLearn.Services.Interfaces
+{
+    public interface IRecommendationService
+    {
+        IEnumerable<RecommendationRuleInfo> GetMatchingRules(
+        int moduleId,
+        int languageId,
+        int questionLevelId,
+        int skillTypeId,
+        int paceTypeId,
+        int? secondaryLanguageId);
+
+        int CreateRecommendationResult(RecommendationResultInfo resultInfo);
+
+        int AddRecommendationResultItem(RecommendationResultItemInfo itemInfo);
+    }
+}
---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Services/RecommendationService.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Dnn.Flow.QuizLearn.Data;
+using Dnn.Flow.QuizLearn.Models;
+using Dnn.Flow.QuizLearn.Services.Interfaces;
+using System.Linq;
+
+namespace Dnn.Flow.QuizLearn.Services
+{
+    public class RecommendationService : IRecommendationService
+    {
+        private readonly IQuizLearnDataRepository _repository;
+        public RecommendationService()
+        {
+            _repository = new SqlDataProvider();
+        }
+
+        public IEnumerable<RecommendationRuleInfo> GetMatchingRules(int moduleId, int languageId, int questionLevelId, int skillTypeId, int paceTypeId, int? secondaryLanguageId)
+        {
+            var exactRules = _repository.FindExactRecommendationRules(
+                moduleId,
+                languageId,
+                questionLevelId,
+                skillTypeId,
+                paceTypeId,
+                secondaryLanguageId);
+
+            if (exactRules != null && exactRules.Any())
+            {
+                return exactRules;
+            }
+
+            var fallbackRules = _repository.FindFallbackRecommendationRules(
+                moduleId,
+                languageId,
+                questionLevelId,
+                skillTypeId,
+                secondaryLanguageId);
+
+            if (fallbackRules != null && fallbackRules.Any())
+            {
+                return fallbackRules;
+            }
+
+            return _repository.FindGeneralRecommendationRules(
+                moduleId,
+                languageId);
+        }
+
+        public int CreateRecommendationResult(RecommendationResultInfo resultInfo)
+        {
+            return _repository.AddRecommendationResult(resultInfo);
+        }
+
+        public int AddRecommendationResultItem(RecommendationResultItemInfo itemInfo)
+        {
+            return _repository.AddRecommendationResultItem(itemInfo);
+        }
+    }
+}
