c798f4b45ea1c463410c7e866a28c19eafab1e94
Zalán Huncsik <zalan.huncsik@stud.uni-corvinus.hu>
2026-04-23 12:26:49 +0200

    RecommendationRuleInfo osztály hozzáadás és az sqldataprovider hibákjavítása

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Models/RecommendationRuleInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dnn.Flow.QuizLearn.Models
+{
+    public class RecommendationRuleInfo
+    {
+        public int RecommendationRuleId { get; set; }
+        public int ModuleId { get; set; }
+        public int LanguageId { get; set; }
+        public int QuestionLevelId { get; set; }
+        public int SkillTypeId { get; set; }
+        public int SecondaryLanguageId { get; set; }
+        public int PaceTypeId { get; set; }
+        public int Priority { get; set; }
+        public string MatchMode { get; set; }
+        public string HotcakesProductSKU { get; set; }
+        public string HotcakesCategoryKey { get; set; }
+        public string HotcakesTag { get; set; }
+        public int MinProducts { get; set; }
+        public int MaxProducts { get; set; }
+    }
+}
