1281bdcbcf8b002144e22da68419e5899b619c99
Zalán Huncsik <zalan.huncsik@stud.uni-corvinus.hu>
2026-04-24 12:34:59 +0200

    QuestionInfo class

---
+++ dnn-flow-quizlearn/Dnn.Flow.QuizLearn/Models/QuestionInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dnn.QuizLearn.Dnn.Flow.QuizLearn.Models
+{
+    public class QuestionInfo
+    {
+        public int QuestionId { get; set; }
+
+        public int LanguageId { get; set; }
+
+        public int QuestionLevelId { get; set; }
+
+        public int SkillTypeId { get; set; }
+
+        public string QuestionText { get; set; }
+
+        public int Points { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
