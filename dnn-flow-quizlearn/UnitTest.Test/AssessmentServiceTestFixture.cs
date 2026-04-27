6fad54c43835b8bec6a42a6fbc0e0e8981c597e8
Dobó H. Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-04-27 14:31:20 +0200

    teszt elkőkészítése

---
+++ dnn-flow-quizlearn/UnitTest.Test/AssessmentServiceTestFixture.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dnn.Flow.QuizLearn.Controllers;
+
+namespace Dnn.Flow.QuizLearn.UnitTest
+{
+    public class AssessmentServiceTestFixture
+    {
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ dnn-flow-quizlearn/UnitTest.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8cb06716-17cf-4481-ba36-04d7d5dff033")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
